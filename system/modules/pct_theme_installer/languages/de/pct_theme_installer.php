e0d3539153521b01a9fbcdbc3b58d886aee02393
Tim Gatzky <info@tim-gatzky.de>
2018-05-17 13:22:19 +0200

    additional information for the import

--- system/modules/pct_theme_installer/languages/de/pct_theme_installer.php
+++ system/modules/pct_theme_installer/languages/de/pct_theme_installer.php
@@ -38,6 +38,7 @@ $GLOBALS['TL_LANG']['PCT_THEME_INSTALLER']['TEMPLATE']['choose_product_info']		=
 $GLOBALS['TL_LANG']['PCT_THEME_INSTALLER']['TEMPLATE']['product_select_blankOption']	= 'Produkt wählen...'; // title
 $GLOBALS['TL_LANG']['PCT_THEME_INSTALLER']['TEMPLATE']['button_systemlog'][0] 		= 'Gehe zu Systemlog...';
 $GLOBALS['TL_LANG']['PCT_THEME_INSTALLER']['TEMPLATE']['welcome_info'] = 'Tragen Sie Ihre Bestellnummer und Bestell-E-Mail-Adresse ein und klicken Sie "Lizenz abfragen". Anschließend können Sie den 1-Click-Installer starten. Das Theme Eclipse wird automatisch vom Premium Contao Themes Server auf Ihren Server geladen und anschließend installiert.';		
+$GLOBALS['TL_LANG']['PCT_THEME_INSTALLER']['TEMPLATE']['sql_template_import_info'] = 'Der SQL-Import kann einige Minuten in Anspruch nehmen. Nach erfolgreichem Import werden Sie zum Contao-Login weitergeleitet.';		
 
 
 /**
