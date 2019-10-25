c1d955e2047d4a38d4e9db1d3ba8a76186a900e1
Tim Gatzky <info@tim-gatzky.de>
2019-10-25 12:37:28 +0200

    update to version 1.3.0

--- system/modules/pct_theme_installer/config/config.php
+++ system/modules/pct_theme_installer/config/config.php
@@ -13,7 +13,7 @@
 /**
  * Constants
  */
-define('PCT_THEME_INSTALLER', '1.2.3');
+define('PCT_THEME_INSTALLER', '1.3.0');
 define('PCT_THEME_INSTALLER_PATH','system/modules/pct_theme_installer');
 
 
