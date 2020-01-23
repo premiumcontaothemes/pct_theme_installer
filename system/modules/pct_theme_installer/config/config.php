156ff058a8b6e6ce4d3d58c57004a283085ef0b5
Tim Gatzky <info@tim-gatzky.de>
2020-01-23 11:01:12 +0100

    #56 updated sql files

--- system/modules/pct_theme_installer/config/config.php
+++ system/modules/pct_theme_installer/config/config.php
@@ -32,11 +32,11 @@ $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipse'] = array
 (
 	'label'	=> 'Eclipse',
 	'zip_folder' => 'eclipse_zip',
-	'mandatory' => array('upload','eclipse contao 3_5.cto','eclipse contao 4_4.cto'), // mandatory zip content on first level
+	'mandatory' => array('upload'), // mandatory zip content on first level
 	'sql_templates' => array
 	(
-		'3.5' => 'eclipse_contao_3_5.sql',
-		'4.4' => 'eclipse_contao_4_4.sql'
+		'4.4' => 'eclipse_contao_4_4.sql',
+		'4.9' => 'eclipse_contao_4_9.sql'
 	),
 );
 $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipse_cc'] = array
@@ -44,11 +44,11 @@ $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipse_cc'] = array
 	'label'	=> 'Eclipse + CustomCatalog Pro',
 	'isCustomCatalog' => true,
 	'zip_folder' => 'eclipse_cc_zip',
-	'mandatory' => array('upload','eclipse_contao_3_5.sql'), // mandatory zip content on first level
+	'mandatory' => array('upload'), // mandatory zip content on first level
 	'sql_templates' => array
 	(
-		'3.5' => 'eclipse_contao_3_5.sql',
-		'4.4' => 'eclipse_contao_4_4.sql'
+		'4.4' => 'eclipse_contao_4_4.sql',
+		'4.9' => 'eclipse_contao_4_9.sql'
 	),
 );
 $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipseX'] = array
@@ -59,7 +59,7 @@ $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipseX'] = array
 	'sql_templates' => array
 	(
 		'4.4' => 'eclipse_contao_4_4.sql',
-		'4.8' => 'eclipse_contao_4_4.sql'
+		'4.9' => 'eclipse_contao_4_9.sql'
 	),
 );
 $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipseX_cc'] = array
@@ -70,7 +70,8 @@ $GLOBALS['PCT_THEME_INSTALLER']['THEMES']['eclipseX_cc'] = array
 	'mandatory' => array('upload'), // mandatory zip content on first level
 	'sql_templates' => array
 	(
-		'4.4' => 'eclipse_contao_4_4.sql'
+		'4.4' => 'eclipse_contao_4_4.sql',
+		'4.9' => 'eclipse_contao_4_9.sql'
 	),
 );
 
