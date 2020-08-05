49b8629a8e81bd63f96a3a6b93258938fc66bd75
Tim Gatzky <info@tim-gatzky.de>
2020-08-05 12:29:32 +0200

    update to version 1.4.2

--- system/modules/pct_theme_installer/config/config.php
+++ system/modules/pct_theme_installer/config/config.php
@@ -13,7 +13,7 @@
 /**
  * Constants
  */
-define('PCT_THEME_INSTALLER', '1.4.1');
+define('PCT_THEME_INSTALLER', '1.4.2');
 define('PCT_THEME_INSTALLER_PATH','system/modules/pct_theme_installer');
 
 
