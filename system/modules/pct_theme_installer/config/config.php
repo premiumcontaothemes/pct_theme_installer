f87bda0f784263686484a655e2aafabdda3ff168
Tim Gatzky <info@tim-gatzky.de>
2026-09-22 09:42:29 +0200

    update to 4.1.0

--- system/modules/pct_theme_installer/config/config.php
+++ system/modules/pct_theme_installer/config/config.php
@@ -16,7 +16,7 @@ use Contao\System;
 /**
  * Constants
  */
-define('PCT_THEME_INSTALLER', '4.0.2');
+define('PCT_THEME_INSTALLER', '4.1.0');
 define('PCT_THEME_INSTALLER_PATH','system/modules/pct_theme_installer');
 
 if( version_compare(ContaoCoreBundle::getVersion(),'5.0','>=') )
